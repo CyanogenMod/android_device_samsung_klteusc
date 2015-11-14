c85eaa9cafea6b7eb87c27efd4177208caf7b731
Ethan Chen <intervigil@gmail.com>
2015-11-14 13:06:01 -0800

    klteusc: Stop using custom mkbootimg
    Change-Id: I94f2965d3cf20d3a02c432fdb33e9c1bb9b96e81

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,6 @@
 TARGET_OTA_ASSERT_DEVICE := klteusc,klte
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/klteusc/mkbootimg.mk
 TARGET_KERNEL_VARIANT_CONFIG := msm8974pro_sec_klte_usc_defconfig
 
 # Init
